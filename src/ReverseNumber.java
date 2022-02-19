2c3346d5100b76cdf7e9ededd32c167c3526b060
ckpd3453 <98898121+ckpd3453@users.noreply.github.com>
2022-02-20 05:23:26 +0530

    Added the Reverse Number Programme

---
+++ src/ReverseNumber.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ReverseNumber {
+
+	public static void main(String[] args) 
+	{
+		try (Scanner scanner = new Scanner(System.in))
+		{
+			System.out.print("Enter the number: ");  
+			int number=scanner.nextInt(); 
+			int reverse = 0;  
+			while(number != 0)   
+			{  
+				int remainder = number % 10;  
+				reverse = reverse * 10 + remainder;  
+				number = number/10;  
+			}  
+			System.out.println("The reverse of the given number is: " + reverse); 
+		}
+	}
+
+}
